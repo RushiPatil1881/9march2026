64c776233796c2a56c73acfb0ff8c170eac79502
rushi <rushi@gmail.com>
2026-03-08 22:25:51 +0000

    111

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
 http.createServer((req,res)=>{
-    res.write("Hello1 from DevOps Pipeline!");
+    res.write("Hello22221 from DevOps Pipeline!");
     res.end();
 }).listen(3000);
